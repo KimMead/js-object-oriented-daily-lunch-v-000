8b6c3ef49c7d69443754e370c0a955471a99e372
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-06-23 17:53:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+let neighborhoodId = 0 
+let customerId = 0 
+let mealId = 0 
+let deliveryId = 0 
+
+class Neighborhood {
+  constructor(name){
+    this.name = name; 
+    this.id = ++neiborhoodId; 
+    store.neighborhood.push(this); 
+  }
+
+  //  returns list of deliveries in a neighborhood 
+  deliveries(){
+    return store.deliveries.filter(delivery => delivery.neighborhood === this.id);
+  }
+  
+  
+}
