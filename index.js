2fe2b0e834b4b1b8aae4122c123e7fc21398b85a
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-06-23 18:05:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,11 +23,29 @@ class Neighborhood {
     return store.customers.filter(customer => customer.neighborhood === this.id);
   }
   // returns list of meals that have been ordered in a neighborhhood
-class Customer {
-  constructor(name, neighborhoodId){
-    this.name = name;
-    this.neighborhoodId = neighborhoodId;
-    this.id = ++customerId;
-    store.customers.push(this);
+  meals(){
+      let meals = this.deliveries().map(delivery => delivery.meal());
+      return meals.filter(function(meal, index, meals) {
+        return meals.indexOf(meal) === index;
+      });
+    }
   }
-}
+  
+  class Customer {
+    constructor(name, neighborhoodId){
+      this.name = name;
+      this.neighborhoodId = neighborhoodId;
+      this.id = ++customerId;
+      store.customers.push(this);
+    }
+  
+  //returns all the deliveries that customer has received
+    deliveries(){
+      return store.deliveries.filter(delivery => delivery.customerId === this.id);
+    }
+  
+  //returns all meals that a customer has ordered
+    meals(){
+      return this.deliveries().map(delivery => delivery.meal())
+    }
+
